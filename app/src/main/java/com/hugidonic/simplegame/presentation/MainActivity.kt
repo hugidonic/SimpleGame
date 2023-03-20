708bc1c1fa2e9a43e4da4eb7b073d98e1632e71a
Hugidonic <netvaca@ya.ru>
2023-03-20 20:00:52 +0300

    MainActivity created

---
+++ app/src/main/java/com/hugidonic/simplegame/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.hugidonic.simplegame.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hugidonic.simplegame.R
+
+class MainActivity : AppCompatActivity() {
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
