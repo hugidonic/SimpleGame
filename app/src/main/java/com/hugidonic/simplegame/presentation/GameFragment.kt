1f9c5c389f737c02463b639ea8d84b3aa342fdd5
Hugidonic <netvaca@ya.ru>
2023-03-24 12:06:49 +0300

    Binding adapters for GameFragment

--- app/src/main/java/com/hugidonic/simplegame/presentation/GameFragment.kt
+++ app/src/main/java/com/hugidonic/simplegame/presentation/GameFragment.kt
@@ -1,19 +1,15 @@
 package com.hugidonic.simplegame.presentation
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.hugidonic.simplegame.databinding.FragmentGameBinding
 import com.hugidonic.simplegame.domain.entity.GameResult
-import com.hugidonic.simplegame.domain.entity.Question
 
 class GameFragment: Fragment() {
 	//	Arguments
@@ -32,17 +28,6 @@ class GameFragment: Fragment() {
 		ViewModelProvider(this, viewModelFactory)[GameViewModel::class.java]
 	}
 
-	private val tvOptions by lazy {
-		mutableListOf<TextView>().apply {
-			add(binding.tvOption1)
-			add(binding.tvOption2)
-			add(binding.tvOption3)
-			add(binding.tvOption4)
-			add(binding.tvOption5)
-			add(binding.tvOption6)
-		}
-	}
-
 	private var _binding: FragmentGameBinding? = null
 	private val binding: FragmentGameBinding
 		get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
@@ -59,69 +44,12 @@ class GameFragment: Fragment() {
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 
-//		Generate question
-		viewModel.question.observe(viewLifecycleOwner) {
-			initQuestion(it)
-			initOptionListeners()
-		}
-
-//		Update timer via viewModel
-		viewModel.formattedTime.observe(viewLifecycleOwner) {
-			binding.tvTimer.text = it
-		}
-
-//		Observe progress of right answers
-		viewModel.percentOfRightAnswers.observe(viewLifecycleOwner) {
-			binding.progressBar.setProgress(it, true)
-		}
-
-//		if percentage is enough it shines green else red
-		viewModel.enoughCount.observe(viewLifecycleOwner) {
-			binding.tvAnswersProgress.setTextColor(getColorByState(it))
-		}
-
-//		if percentage is enough it shines green else red
-		viewModel.enoughPercent.observe(viewLifecycleOwner) {
-			binding.progressBar.progressTintList = ColorStateList.valueOf(getColorByState(it))
-		}
-
-//		Secondary progress
-		viewModel.minPercent.observe(viewLifecycleOwner) {
-			binding.progressBar.secondaryProgress = it
-		}
+		binding.viewModel = viewModel
+		binding.lifecycleOwner = viewLifecycleOwner
 
 //		Ending the game and launch finish screen
 		viewModel.gameResult.observe(viewLifecycleOwner) {
 			launchGameFinishedFragment(it)
-		}
-
-		viewModel.progressAnswers.observe(viewLifecycleOwner) {
-			binding.tvAnswersProgress.text = it
-		}
-	}
-
-	private fun getColorByState(isEnough: Boolean): Int {
-		val colorResId = if (isEnough) {
-			android.R.color.holo_green_light
-		} else  {
-			android.R.color.holo_red_light
-		}
-		return ContextCompat.getColor(requireContext(), colorResId)
-	}
-
-	private fun initQuestion(question: Question) = with(binding) {
-		tvSum.text = question.sum.toString()
-		tvLeftNumber.text = question.visibleNumber.toString()
-		for (i in 0 until tvOptions.size) {
-			tvOptions[i].text = question.options[i].toString()
-		}
-	}
-
-	private fun initOptionListeners() {
-		for (i in 0 until tvOptions.size) {
-			tvOptions[i].setOnClickListener {
-				viewModel.chooseAnswer(tvOptions[i].text.toString().toInt())
-			}
 		}
 	}
 
--- app/src/main/java/com/hugidonic/simplegame/presentation/binding/BindingAdapters.kt
+++ app/src/main/java/com/hugidonic/simplegame/presentation/binding/BindingAdapters.kt
@@ -1,10 +1,19 @@
 package com.hugidonic.simplegame.presentation.binding
 
+import android.content.Context
+import android.content.res.ColorStateList
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import com.hugidonic.simplegame.R
 import com.hugidonic.simplegame.domain.entity.GameResult
+
+
+interface OnOptionClickListener {
+	fun onOptionClick(option: Int)
+}
 
 @BindingAdapter("requiredAnswers")
 fun bindRequiredAnswers(textView: TextView, count: Int) {
@@ -55,4 +64,36 @@ fun bindEmojiResult(imageView: ImageView, isWinner: Boolean) {
 		R.drawable.ic_sad
 	}
 	imageView.setImageResource(smileImageId)
+}
+
+@BindingAdapter("progressBarColor")
+fun bindProgressBarColor(progressBar: ProgressBar, enoughPercent: Boolean) {
+	progressBar.progressTintList = ColorStateList.valueOf(
+		getColorByState(progressBar.context, enoughPercent)
+	)
+}
+@BindingAdapter("answersProgressTextColor")
+fun bindProgressBarColor(textView: TextView, enoughCount: Boolean) {
+	textView.setTextColor(getColorByState(textView.context, enoughCount))
+}
+
+private fun getColorByState(context: Context, isEnough: Boolean): Int {
+	val colorResId = if (isEnough) {
+		android.R.color.holo_green_light
+	} else  {
+		android.R.color.holo_red_light
+	}
+	return ContextCompat.getColor(context, colorResId)
+}
+
+@BindingAdapter("numberAsText")
+fun bindNumberAsText(textView: TextView, number: Int) {
+	textView.text = number.toString()
+}
+
+@BindingAdapter("onOptionClickListener")
+fun bindOnOptionClickListener(textView: TextView, clickListener: OnOptionClickListener) {
+	textView.setOnClickListener {
+		clickListener.onOptionClick(textView.text.toString().toInt())
+	}
 }
